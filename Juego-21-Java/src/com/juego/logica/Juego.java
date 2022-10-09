b287fbe0276a94debddd3caecd78b23902fa2da3
Jose Ramon <j.ramonroz@gmail.com>
2022-10-08 21:12:29 -0500

    Base del proyecto

---
+++ Juego-21-Java/src/com/juego/logica/Juego.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.juego.logica;
+
+/**
+ *
+ * @author rami
+ */
+public class Juego {
+
+    //Crear variables utilizadas por la clase Juego
+    private int wins;
+    private int losses;
+    private int pushes;
+
+    //Constructos de la clase
+    public Juego() {
+        wins = 0;
+        losses = 0;
+        pushes = 0;
+    }
+}
